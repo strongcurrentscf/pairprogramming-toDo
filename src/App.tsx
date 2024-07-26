c96cfcd0a89729ffc3141e7d7b3c370861c07783
Christian Francis <cfrancis1989@gmail.com>
2024-07-26 19:26:13 -0400

    AddToDo Logic & Functionality added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 // toDo-object type
 type ToDoProps = {
-  id: number;
-  title: string;
+  id: string;
+  text: string;
   status: string;
 };
 
@@ -18,8 +18,8 @@ enum STATUS {
 }
 
 // ToDo component
-const ToDo: React.FC<{ toDo: ToDoProps }> = (props) => {
-  return <li key={props.toDo.id}>{props.toDo.title}</li>;
+const ToDo: React.FC<{ text: string }> = (props) => {
+  return <li>{props.text}</li>;
 };
 
 // List component
@@ -45,34 +45,95 @@ const List: React.FC<{ list: ListProp; status: STATUS }> = (props) => {
         border: "5px solid black",
       }}
     >
-      <h1 style={{ background: `${color}` }}>{props.status.toUpperCase()}</h1>
+      <h1 style={{ background: color }}>{props.status.toUpperCase()}</h1>
       <ul>
         {filteredList.map((toDo) => (
-          <ToDo toDo={toDo} />
+          <ToDo key={toDo.id} text={toDo.text} />
         ))}
       </ul>
     </div>
   );
 };
 
+// Input component
+const NewTodo: React.FC<{ onAddToDo: (text: string) => void }> = (props) => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      return;
+    }
+
+    props.onAddToDo(enteredText);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Todo text</label>
+      <input type="text" id="text" ref={todoTextInputRef}></input>
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
 // Root app component
 function App() {
+  // DUMMMY DATA
+  // const toDoInitialDummyList: ListProp = [
+  //   { id: 1, text: "Task 1", status: STATUS.PENDING },
+  //   { id: 2, text: "Task 2", status: STATUS.DONE },
+  //   { id: 3, text: "Task 3", status: STATUS.INPROGRESS },
+  //   { id: 4, text: "Task 4", status: STATUS.PENDING },
+  //   { id: 5, text: "Task 5", status: STATUS.DONE },
+  //   { id: 6, text: "Task 6", status: STATUS.INPROGRESS },
+  //   { id: 7, text: "Task 7", status: STATUS.PENDING },
+  //   { id: 8, text: "Task 8", status: STATUS.DONE },
+  //   { id: 9, text: "Task 9", status: STATUS.INPROGRESS },
+  //   { id: 10, text: "Task 10", status: STATUS.PENDING },
+  //   { id: 11, text: "Task 11", status: STATUS.DONE },
+  //   { id: 12, text: "Task 12", status: STATUS.INPROGRESS },
+  //   { id: 13, text: "Task 13", status: STATUS.PENDING },
+  //   { id: 14, text: "Task 14", status: STATUS.DONE },
+  //   { id: 15, text: "Task 15", status: STATUS.INPROGRESS },
+  // ];
+
+  // STATE
+  const [toDoList, setToDoList] = useState<ToDoProps[]>([]);
+
+  // Handler functions
   const changeStatus = () => {
     // onClick, take id from clicked toDo  and change status
   };
 
-  const addTodo = () => {
-    //
+  const addToDo = (toDoText: string) => {
+    const newToDo: ToDoProps = {
+      id: new Date().toISOString(),
+      text: toDoText,
+      status: STATUS.PENDING,
+    };
+
+    setToDoList((prevTodos) => {
+      return prevTodos.concat(newToDo);
+    });
   };
 
-  const removeTodo = () => {};
+  const removeToDo = (toDoId: number) => {
+    const toDoListIndex = toDoList.findIndex((toDo) => toDo.id === toDoId);
 
-  // STATE
-  const [toDoList, setToDoList] = useState<ListProp | []>([]);
+    const toDoListCopy = [...toDoList];
+    toDoListCopy.splice(toDoListIndex, 1);
+
+    setToDoList(toDoListCopy);
+  };
 
   return (
     // Need an input component for toDo strings data
     <>
+      <NewTodo onAddToDo={addToDo} />
       <div style={{ display: "flex", height: "1000px", width: "100%" }}>
         <List list={toDoList} status={STATUS.PENDING} />
         <List list={toDoList} status={STATUS.INPROGRESS} />
